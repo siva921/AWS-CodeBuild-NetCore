80337532060ed3f00b2c323585e1a74a5b4a74df
siva921 <60959198+siva921@users.noreply.github.com>
2020-03-06 16:35:20 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+code coverage == yes
+
+if [ code coverage == yes ] 
+then 
+    #If they are equal then print this 
+    echo "build will pass"
+else
+    #else print this 
+    echo "build fail"
+fi 
